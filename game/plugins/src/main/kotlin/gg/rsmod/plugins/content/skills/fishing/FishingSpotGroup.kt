6b1b384bb37ff0fa23765430ed6411cb2b88ec9d
Paleocene <>
2023-02-24 22:51:42 +0100

    wip

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/fishing/FishingSpotGroup.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/fishing/FishingSpotGroup.kt
@@ -1,10 +1,18 @@
 package gg.rsmod.plugins.content.skills.fishing
 
-import net.runelite.cache.definitions.MapDefinition.Tile
+import gg.rsmod.game.model.Tile
 
-enum class FishingSpotGroup(val test: Int, vararg val locations: Tile) {
+enum class FishingSpotGroup(val tiles: List<Tile>) {
+    LUMBRIDGE_RIVER(listOf(Tile(3239, 3241), Tile(3239, 3242), Tile(3239, 3243), Tile(3239, 3244), Tile(3238, 3251), Tile(3238, 3252), Tile(3238, 3254), Tile(3238, 3255)))
+    ;
 
+    fun newTile(previous: Tile): Tile {
+        return (tiles - previous).random()
+    }
+
+    companion object {
+        fun forTile(tile: Tile): FishingSpotGroup? {
+            return values().firstOrNull { tile in it.tiles }
+        }
+    }
 }
-
-
-// LUMBRIDGE(new Location(3239, 3243, 0), new Location(3239, 3241, 0), new Location(3239, 3242, 0), Location.create(3238, 3251, 0), Location.create(3238, 3252, 0), Location.create(3238, 3253, 0)),
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/fishing/fishing.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/fishing/fishing.plugin.kts
@@ -1,5 +1,12 @@
 package gg.rsmod.plugins.content.skills.fishing
 
+import gg.rsmod.plugins.content.magic.TeleportType
+import gg.rsmod.plugins.content.magic.teleport
+
+val MOVEMENT_TIMER = TimerKey()
+//val MOVEMENT_DELAY = 280..530
+val MOVEMENT_DELAY = 15..15
+
 FishingSpot.values().forEach { spot ->
     spot.objectIds.forEach { spotId ->
         spot.tools.forEach { tool ->
@@ -12,5 +19,21 @@ FishingSpot.values().forEach { spot ->
                 }
             }
         }
+    }
+}
+
+FishingSpot.values().forEach { spot ->
+    spot.objectIds.forEach { spotId ->
+        on_npc_spawn(spotId) {
+            npc.timers[MOVEMENT_TIMER] = world.random(MOVEMENT_DELAY)
+        }
+    }
+}
+
+on_timer(MOVEMENT_TIMER) {
+    FishingSpotGroup.forTile(npc.tile)?.let { group ->
+        npc.moveTo(group.newTile(npc.tile))
+//        npc.walkTo(group.newTile(npc.tile), detectCollision = false)
+        npc.timers[MOVEMENT_TIMER] = world.random(MOVEMENT_DELAY)
     }
 }
